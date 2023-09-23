e46bf5d7540b52d3586857e3cd6fb747b78b2a39
Anatom <49984698+anatom3000@users.noreply.github.com>
2023-09-23 22:13:59 +0200

    simple reflexion

--- main.py
+++ main.py
@@ -2,94 +2,230 @@ from __future__ import annotations
 
 from collections import namedtuple
 from dataclasses import dataclass
-from math import sin
+import math
+from functools import cached_property
 
 import pygame
 from pygame.locals import *
 
-RESOLUTION = (640, 480)
+from math import inf
+
+RESOLUTION = (1280, 720)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GRAY = (150, 150, 150)
+POINT_SIZE = 2
 
-Point = namedtuple("Point", ['x', 'y'])
-Vector = namedtuple("Vector", ['x', 'y'])
+Point = Vector = pygame.Vector2
 
+import sys
+
+sys.setrecursionlimit(1000)
 
 @dataclass
 class Ray:
     origin: Point
     direction: Vector
+    reflected: bool = False
+
+    def draw(self, surface: pygame.Surface):
+        VERY_BIG = 2*max(RESOLUTION)
+
+        pygame.draw.line(surface, (255, 0, 0) if self.reflected else GRAY, self.origin, self.origin+self.direction.normalize()*VERY_BIG)
+        draw_point(surface, self.origin)
+
+
+@dataclass
+class Interface:
+    start: Point
+    end: Point
+    front_indice: float = 1.0
+    back_indice: float = 1.0
+
+    @cached_property
+    def director(self) -> Vector:
+        return (self.end - self.start).normalize()
+
+    @cached_property
+    def normal(self) -> Vector:
+        # normal is pointing up when start is to the left of end
+        #    ↑n
+        # A---->B
+        return Vector(-self.director.y, self.director.x)
+
+    @cached_property
+    def normal_angle(self) -> float:
+        return self.normal.as_polar()[1]
+
+    def refract(self, direction: Vector, at: Point) -> Optional[Ray]:
+        dir_amp, dir_angle = direction.as_polar()
+
+        i1 = self.normal_angle - dir_angle
+
+        refracted_ray = Vector.from_polar((1.0, 180+2*self.normal_angle-dir_angle))
+
+        return Ray(origin=at, direction=refracted_ray, reflected=False)
+
+    def draw(self, surface: pygame.Surface):
+        pygame.draw.line(surface, GRAY, self.start, self.end)
+
+        draw_point(surface, self.start, (0, 255, 0))
+        draw_point(surface, self.end, (255, 0, 255))
+
 
 class RayCaster:
-    def __init__(self, segment: (Point, Point)):
-        self.segment = segment
+    def __init__(self, interfaces: list[Interface]):
+        self.interfaces = interfaces
 
-    def raycast(self, ray: Ray) -> Optional[Point]:
-        a = self.segment[0]
-        b = self.segment[1]
-        o = ray.origin
-        r = ray.direction
+    def project(self, *rays: Ray, limit: int = sys.getrecursionlimit()-100, ignore: int = -1) -> list[Ray]:
+        if limit == 0:
+            return list(rays)
 
-        determinant = (a.y - b.y)*r.x - (a.x - a.x)*r.y
+        o = rays[-1].origin
+        r = rays[-1].direction
 
-        if determinant == 0:
-            return None
+        intersec_t = math.inf
+        intersected_interface = None
+        for index, interf in enumerate(self.interfaces):
+            if index == ignore:
+                continue
 
-        t = (a.y - b.y)*(a.x - o.x) + (a.y - o.y)*(b.x-a.x)
-        s = (o.x - a.x)*r.y + (a.y - o.y)*r.x
+            a = interf.start
+            b = interf.end
 
-        t /= determinant
-        s /= determinant
+            determinant = (a.y - b.y) * r.x - (a.x - b.x) * r.y
 
-        if not (0 < s < 1 and t > 0):
-            return None
+            if determinant == 0:
+                continue
 
-        return Point(
-            x=o.x + r.x * t,
-            y=o.y + r.y * t
+            t = (a.y - b.y) * (a.x - o.x) + (a.y - o.y) * (b.x - a.x)
+            s = (a.y - o.y) * r.x - (a.x - o.x) * r.y
+
+            t /= determinant
+            s /= determinant
+
+            if not (0 < s < 1 and t > 0):
+                continue
+
+            if t < intersec_t:
+                intersec_t = t
+                intersected_interface = index
+
+        if math.isinf(intersec_t):
+            return list(rays)
+
+        refracted = self.interfaces[intersected_interface].refract(
+            direction=rays[-1].direction,
+            at=Point(
+                x=o.x + r.x * intersec_t,
+                y=o.y + r.y * intersec_t
+            )
         )
 
+        if refracted is None:
+            return list(rays)
 
-def draw_point(surface: pygame.Surface, point: Point):
-    pygame.draw.circle(surface, WHITE, (point.x, point.y), 5)
+        return self.project(
+            *rays,
+            refracted,
+            limit=limit-1,
+            ignore=intersected_interface
+        )
+
+    def draw(self, surface: pygame.Surface):
+        for interf in self.interfaces:
+            interf.draw(surface)
+
+    def dump_interfaces(self):
+        print("BEGIN INTERFACE DUMP")
+        for interf in self.interfaces:
+            print(f"Interface(start=Point({interf.start.x}, {interf.start.y}), " 
+                  f"end=Point({interf.end.x}, {interf.end.y}), "
+                  f"front_indice={interf.front_indice}, back_indice={interf.back_indice}),")
+        print("END INTERFACE DUMP")
+
+def draw_point(surface: pygame.Surface, point: Point, color: (int, int, int) = WHITE):
+    pygame.draw.circle(surface, color, point, POINT_SIZE)
 
 
-def draw_segment(surface: pygame.Surface, segment: (Point, Point)):
-    pygame.draw.line(surface, GRAY, segment[0], segment[1])
+def draw_segment(surface: pygame.Surface, ray: Ray, end: Point):
+    pygame.draw.line(surface, (255, 0, 0) if ray.reflected else GRAY, ray.origin, end)
 
-    draw_point(surface, segment[0])
-    draw_point(surface, segment[1])
-
+    draw_point(surface, ray.origin)
+    draw_point(surface, end)
 
 screen = pygame.display.set_mode(RESOLUTION)
 clock = pygame.time.Clock()
 
-raycaster = RayCaster(segment=(Point(400.0, 100.0), Point(400.0, 400.0)))
+ray = Ray(
+    origin=Point(RESOLUTION[0].__float__(), RESOLUTION[1]/2),
+    direction=Vector(-1.0, 0.0),
+)
+
+raycaster = RayCaster([
+])
 
 t = 0.0
+paused = False
 running = True
+point_buffer = None
+ray_start = None
+
 while running:
-    t += clock.tick(60) / 1000
+    t += clock.tick(60) / 1000 * (not paused)
 
     for ev in pygame.event.get():
         if ev.type == QUIT:
             running = False
             break
+        if ev.type == KEYUP:
+            if ev.key == K_SPACE:
+                paused = not paused
+            if ev.key == K_d:
+                raycaster.dump_interfaces()
+            if ev.key == K_c:
+                if point_buffer is not None:
+                    point_buffer = None
+                if len(raycaster.interfaces) != 0:
+                    raycaster.interfaces.pop()
+
+        if ev.type == MOUSEBUTTONDOWN:
+            if ev.button == 1:
+                if point_buffer is None:
+                    point_buffer = ev.pos
+                else:
+                    raycaster.interfaces.append(Interface(
+                        Point(*point_buffer),
+                        Point(*ev.pos),
+                        front_indice=inf
+                    ))
+                    point_buffer = None
+            if ev.button == 3:
+                ray_start = ev.pos
+
+        if ev.type == MOUSEBUTTONUP:
+            if ev.button == 3:
+                diff = Vector(ev.pos)-Vector(ray_start)
+                if diff != Vector(0.0, 0.0):
+                    ray = Ray(origin=Vector(ray_start), direction=diff)
+
 
     screen.fill(BLACK)
 
-    ray = Ray(
-        origin=Point(100.0, 250.0),
-        direction=Vector(50.0, 30.0*sin(t))
-    )
-    casted = raycaster.raycast(ray)
+    refracted = raycaster.project(ray)
 
-    draw_segment(screen, raycaster.segment)
+    raycaster.draw(screen)
 
-    if casted is None:
-        draw_point(screen, ray.origin)
+    if len(refracted) == 1:
+        refracted[0].draw(screen)
     else:
-        draw_segment(screen, (ray.origin, casted))
+        next_ray = None
+        for refracted_ray, next_ray in zip(refracted[:-1], refracted[1:]):
+            draw_segment(screen, refracted_ray, next_ray.origin)
+
+        next_ray.draw(screen)
+
+    if point_buffer is not None:
+        draw_point(screen, point_buffer)
 
     pygame.display.flip()
